be1fbb66d4434e5d2263f4d4dc2fa9f59f122268
byju-poorvika <poorvika.khanna@byjus.com>
2024-01-18 15:36:39 +0530

    Started the app

---
+++ src/test/java/com/mycompany/Property_Management/PropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.Property_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
